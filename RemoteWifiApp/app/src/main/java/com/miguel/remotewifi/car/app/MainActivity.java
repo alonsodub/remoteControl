7fcbcf4dc24ce84f8fad0a11c93109330b76462a
Alejandro Linn <jesuislinn@gmail.com>
2016-10-27 10:40:53 -0700

    First push of app's project code structure

---
+++ RemoteWifiApp/app/src/main/java/com/miguel/remotewifi/car/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.miguel.remotewifi.car.app;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
